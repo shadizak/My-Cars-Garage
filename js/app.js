e208e9101ab40a4161d8416a4dbf98205e74cab1
Shadi Zaqout <shadizaqout@gmail.com>
2020-07-07 12:00:31 +0300

    add file Car Exam

---
+++ js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+ 
+
+  var allCar = [];
+
+  function Car(model , year, manufacturer){
+      this.model = model;
+      this.year = year;
+      this.manufacturer = manufacturer;
+       this.price = randomIntFromInterval(7000,100000);
+ 
+      allCar.push(this);
+  }
+
+
+
+  function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
